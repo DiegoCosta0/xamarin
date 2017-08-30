ec74120c5f282f4e9cf267f35d1fa530baf92c82
diego <diegofernandes9506@gmail.com>
2017-08-29 22:20:27 -0300

    primeiro programa com xamarin

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -12,6 +12,71 @@ namespace App2
 		public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            sum.Clicked += delegate
+            {
+                int val1 = int.Parse(value1.Text);
+                int val2 = int.Parse(value2.Text);
+
+                int resultado = Matematica.Soma(val1, val2);
+
+                result.Text = "O resultado é " + resultado;
+
+            };
+
+            multiple.Clicked += delegate
+            {
+                int val1 = int.Parse(value1.Text);
+                int val2 = int.Parse(value2.Text);
+
+                int resultado = Matematica.Multiplicar(val1, val2);
+
+                result.Text = "O resultado da multiplicação é " + resultado;
+            };
+
+            sub.Clicked += delegate
+            {
+                int val1 = int.Parse(value1.Text);
+                int val2 = int.Parse(value2.Text);
+
+                int resultado = Matematica.Subtracao(val1, val2);
+
+                result.Text = "O resultado da subtração é " + resultado;
+            };
+
+            div.Clicked += delegate
+            {
+                int val1 = int.Parse(value1.Text);
+                int val2 = int.Parse(value2.Text);
+
+                float resultado = Matematica.Divisao(val1, val2);
+
+                result.Text = "O resultado da divisão é " + resultado;
+            };
+
+        }
+
+        private class Matematica
+        {
+            public static int Soma(int a, int b)
+            {
+                return a + b;
+            }
+
+            public static int Multiplicar(int a, int b)
+            {
+                return a * b;
+            }
+
+            public static int Subtracao(int a, int b)
+            {
+                return a - b;
+            }
+
+            public static float Divisao(float a, float b)
+            {
+                return a / b;
+            }
+        }
+    }
 }
